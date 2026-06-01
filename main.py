ed7ae53a0bee602ed202d399bdd997bf27ad3daf
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-06-01 17:14:04 +0300

    Modified the evaluation metrics
    - Corrected the recall metric.
    For each find query the number of calculated nearest neighbors
    that are actual nearest neighbors.
    Add that number across all queries amd divide by
    (number of queries * number of nearest neigbors calculated per query)
    
    - Changed the queries per second metric to take as input the number
    of queries instead of a matrix of query vectors

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ from config import (
     N_NEAREST_NEIGHBORS,
     PCA_DIMENSIONS,
     SIFT_BASE,
+    SIFT_GROUNDTRUTH,
     SIFT_LEARN,
     SIFT_QUERY,
 )
 from model.inverted_index import InvertedIndex
 from utils.clustering import get_cluster_info
-from utils.dataset_loader import read_fvecs
+from utils.dataset_loader import read_fvecs, read_ivecs
+from utils.evaluation_utils import queries_per_second, recall
 from utils.inverted_index_utils import build_indexes
 
 
 def main():
-    dataset = read_fvecs(SIFT_LEARN)
-    # dataset = read_fvecs(SIFT_BASE)
+    # dataset = read_fvecs(SIFT_LEARN)
+    dataset = read_fvecs(SIFT_BASE)
     queries = read_fvecs(SIFT_QUERY)
+    groundtruth = read_ivecs(SIFT_GROUNDTRUTH)
 
     pca_start_time = time.perf_counter()
     pca = PCA(n_components=PCA_DIMENSIONS)
@@ -45,7 +48,7 @@ def main():
     )
     print(f"Finished building index in {time.perf_counter() - index_start_time}")
 
-    neighbors_start_time = time.perf_counter()
+    ann_start_time = time.perf_counter()
     neighbors, n_distances_calculated = find_nearest_neighbors(
         queries=queries[:20],
         n_nearest_neighbors=N_NEAREST_NEIGHBORS,
@@ -54,8 +57,18 @@ def main():
         inverted_indexes=inverted_indexes,
         matrix=dataset,
     )
+    ann_end_time = time.perf_counter()
 
-    print(f"Finished finding neighbors {time.perf_counter() - neighbors_start_time}")
+    recall_1: float = recall(
+        neighbors[0].reshape(1, -1), groundtruth[0].reshape(1, -1), neighbors.shape[1]
+    )
+    qps = queries_per_second(1, ann_end_time - ann_start_time)
+
+    print(f"{qps=}")
+
+    print(f"Recall for the 1st query is: {recall_1}")
+
+    print(f"Finished finding neighbors {ann_end_time - ann_start_time}")
     print(
         f"Number of distances calculated for the 1st query: {n_distances_calculated[0]}\n"
     )
--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -1,42 +1,44 @@
-from typing import List
-
-from numpy import float32, int64
+from numpy import int64
+import numpy as np
 from numpy.typing import NDArray
 
 
-def recall(nearest_neighbors: NDArray[int64], groundtruth: NDArray[int64]) -> float:
+def recall(
+    nearest_neighbors: NDArray[int64], groundtruth: NDArray[int64], n_neighbors: int
+) -> float:
     """
     Returns the calculated recall of the nearest neighbor algorithm.
-    The recall is calculated based on the number of neighbors that
-    were correctly guessed for each query.
+
+    The recall is calculated as:
+    (The sum of all the correctly guessed nearest neighbors per query) /
+    (the number of queries * number of nn per query)
+
+    The order of the calculated and the actual nearest neighbors
+    does not matter.
 
     The i-th element of `nearest_neighbors` and the
     i-th element of `groundtruth` must refer to the same query.
 
-    For both arrays the indices of the nearest neighbor for each query
-    must be order based on ascending distance between the neighbor
-    and the query.
-
     Args:
         nearest_neighbors (NDArray[int64]):
-            A 2D array where the i-th element are the indices
+            A 2D array where the i-th row has the indices
             of the nearest neighbors of the i-th query as calculated
             by the algorithm.
-
         groundtruth (NDArray[int64]):
-            A 2D array where the i-th element are the indices
-            of the nearest neighbors of the i-th query.
+            A 2D array where the i-th row has the indices
+            of the actual nearest neighbors of the i-th query.
+        n_neighbors (int): The number of neighbors that were calculated.
 
     Returns:
         float: The calculated recall of the nearest neighbor algorithm.
     """
 
-    gd_n_queries, gd_n_neighbors = groundtruth.shape
+    gt_n_queries, gd_n_neighbors = groundtruth.shape
     nn_n_queries, nn_n_neighbors = nearest_neighbors.shape
 
-    if nn_n_queries > gd_n_queries:
+    if nn_n_queries > gt_n_queries:
         raise ValueError(
-            f"Nearest neighbors array is has more queries than groundtruth array ({nn_n_queries} > {gd_n_queries})"
+            f"Nearest neighbors array is has more queries than groundtruth array ({nn_n_queries} > {gt_n_queries})"
         )
 
     if nn_n_neighbors > gd_n_neighbors:
@@ -44,37 +46,27 @@ def recall(nearest_neighbors: NDArray[int64], groundtruth: NDArray[int64]) -> fl
             f"Nearest neighbors array has more neighbors per query than groundtruth array ({nn_n_neighbors} > {gd_n_neighbors})"
         )
 
-    correct_guesses: int = 0
-    for predicted, true in zip(nearest_neighbors, groundtruth):
-        true_set = set(true)
+    # loop over every query and find the number of common elements
+    # between the predicted nearest neighbors and the actual nn.
+    # Calculate the sum of correct elements over all queries.
+    correct_guesses = sum(
+        np.isin(predicted, gt).sum()
+        for predicted, gt in zip(nearest_neighbors, groundtruth)
+    )
 
-        for neighbor in predicted:
-            if neighbor in true_set:
-                correct_guesses += 1
-
-    return correct_guesses / (nn_n_queries * nn_n_neighbors)
-
-
-def recall_at_k(pred, gt, k=100):
-    hits = 0
-    for p, g in zip(pred, gt):
-        if g[0] in p[:k]:
-            hits += 1
-    return hits / len(pred)
+    return correct_guesses / (gt_n_queries * n_neighbors)
 
 
-def queries_per_second(queries: NDArray[float32], time_per_query: List[float]) -> float:
+def queries_per_second(n_queries: int, total_time: float) -> float:
     """
-    Calculates the queries per second for which the nearest neighbors are found.
+    Calculates the number of queries for which the nearest neighbors were found
+    in a second.
 
     Args:
-        queries (NDArray): Contain
-        time (List[float]): Each float is the time it took
-            to find the nearest neighbors i-th element of `queries`
+        n_queries (int): Number of queries used.
+        total_time (List[float]): The time it took to finish
+            the calculations for all the queries.
     Returns:
         float: Queries per second
     """
-    n_queries = len(queries)
-    total_time = sum(time_per_query)
-
     return n_queries / total_time
