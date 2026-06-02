b977b0520c87aac9efdcf0f496171dce5f7a50ca
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-06-02 22:12:15 +0300

    Removed functions
    Removed functions used to sort the cluster members inside of an inverted
    index. They were not used anymore

--- utils/inverted_index_utils.py
+++ utils/inverted_index_utils.py
@@ -5,7 +5,6 @@ from numpy import float32, int64
 from numpy.typing import NDArray
 
 from model.inverted_index import InvertedIndex
-from utils.distance import euclidian_distance
 
 
 def build_inverted_indexes(
@@ -49,38 +48,3 @@ def build_inverted_indexes(
         inverted_indexes.append(inv_ind)
 
     return inverted_indexes
-
-
-def _sort_indexes(
-    centroid: NDArray[float32],
-    cluster_member_indexes: NDArray[int64],
-    matrix: NDArray[float32],
-) -> NDArray[int64]:
-    """
-    Sorts `cluster_member_indexes` items in descending order
-    based on the distance of each item from the centroid.
-
-    Args:
-        centroid (NDArray[float32]): The centroid of the cluster
-        cluster_member_indices (NDArray[int64]): Array containing the indexes
-            of the each cluster member based on the original matrix.
-        matrix (NDArray[float32]): The matrix containing all the vectors
-            of the dataset
-
-    Returns:
-        NDArray[int64]: The `cluster_member_indexes` array sorted
-            based on each member's distance from the centroid
-    """
-    distances = _dist(centroid, cluster_member_indexes, matrix)
-
-    return cluster_member_indexes[np.argsort(distances)]
-
-
-def _dist(
-    centroid: NDArray[float32],
-    cluster_member_indices: NDArray[int64],
-    matrix: NDArray[float32],
-):
-    return euclidian_distance(
-        centroid=centroid, cluster_member_indices=cluster_member_indices, matrix=matrix
-    )
